0f52acd13df533a4816d77062110ff26eaa72454
Aristides Messias Jr <aristides.messias@kahumanas.com.br>
2020-04-24 22:03:54 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/estudoudemy/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.estudoudemy.cursomc.repositories;
+
+import com.estudoudemy.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
