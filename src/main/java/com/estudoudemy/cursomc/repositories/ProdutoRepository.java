3e35c3480b4d364800366612d1424706abdc7014
Aristides Messias Jr <aristides.messias@kahumanas.com.br>
2020-04-27 21:09:47 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/estudoudemy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.estudoudemy.cursomc.repositories;
+
+import com.estudoudemy.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
