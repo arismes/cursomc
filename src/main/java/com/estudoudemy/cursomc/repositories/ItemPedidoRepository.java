29f841fc06ffd818bb5f212049d4a1f5dbd2b093
Aristides Messias Jr <aristides.messias@kahumanas.com.br>
2020-05-06 20:02:32 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/estudoudemy/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.estudoudemy.cursomc.repositories;
+
+import com.estudoudemy.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
