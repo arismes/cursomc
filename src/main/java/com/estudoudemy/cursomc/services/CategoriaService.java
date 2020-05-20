c3ec0b1045d62b2c0a484524c63aabf9a8432ce5
Aristides Messias Jr <aristides.messias@kahumanas.com.br>
2020-05-20 15:19:28 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/estudoudemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudoudemy/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.estudoudemy.cursomc.services;
 
 import com.estudoudemy.cursomc.domain.Categoria;
 import com.estudoudemy.cursomc.repositories.CategoriaRepository;
+import com.estudoudemy.cursomc.services.exceptions.DataIntegrityException;
 import com.estudoudemy.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,5 +31,14 @@ public class CategoriaService {
     public Categoria update(Categoria obj){
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try {
+            repo.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+        }
     }
 }
